e02a676dde737a7859a2e8e3cb46c8328f75822e
wilsontbx <boonxian1993@hotmail.com>
2021-01-26 11:25:13 +0800

    updated some scenario

--- test/findMultiples.test.js
+++ test/findMultiples.test.js
@@ -3,3 +3,11 @@ const findMultiples = require("../src/findMultiples");
 test("This should return array 2,4,6", function () {
   expect(findMultiples(2, 6)).toEqual([2, 4, 6]);
 });
+
+test("This should return array 3,6,9", function () {
+  expect(findMultiples(3, 10)).toEqual([3, 6, 9]);
+});
+
+test("This should return array 5", function () {
+  expect(findMultiples(5, 6)).toEqual([5]);
+});
